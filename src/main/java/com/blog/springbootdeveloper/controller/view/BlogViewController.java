7b2f4718610c398cb50d2d5d8694be9ba0161bd1
YoungJinKim <youngjin1293@naver.com>
2025-05-31 12:38:26 +0900

    Update Create

--- src/main/java/com/blog/springbootdeveloper/controller/view/BlogViewController.java
+++ src/main/java/com/blog/springbootdeveloper/controller/view/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,7 +19,7 @@ public class BlogViewController {
     private final BlogService blogService;
 
     @GetMapping("/articles")
-    public String getArticles(Model model){
+    public String getArticles(Model model) {
         List<ArticleListViewResponse> articles = blogService.findAll()
                 .stream()
                 .map(ArticleListViewResponse::new)
@@ -28,9 +29,24 @@ public class BlogViewController {
     }
 
     @GetMapping("/articles/{id}")
-    public String getArticle(@PathVariable Long id, Model model){
+    public String getArticle(@PathVariable Long id, Model model) {
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleViewReponse(article));
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        // id 가 없으면 빈 ArticleViewResponse를 반환
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewReponse());
+        }
+        else {
+        // id 가 있으면 Article 조회 후 넘기기
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewReponse(article));
+        }
+
+        return "newArticle";
     }
 }
